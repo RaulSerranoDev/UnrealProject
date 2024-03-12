bdde53efb4a7f6ec234340065e4d98e5c1ed536d
RaulSerranoDev <raulserranodev@gmail.com>
2024-03-12 10:51:19 +0100

    Movement Input

--- Source/Aura/Private/Player/GamePlayerController.cpp
+++ Source/Aura/Private/Player/GamePlayerController.cpp
@@ -3,7 +3,10 @@
 
 #include "Player/GamePlayerController.h"
 
-#include "EnhancedInputSubsystems.h"
+#include <EnhancedInputSubsystems.h>
+#include <EnhancedInputComponent.h>
+#include <InputAction.h>
+#include <InputActionValue.h>
 
 AGamePlayerController::AGamePlayerController()
 {
@@ -26,4 +29,30 @@ void AGamePlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AGamePlayerController::SetupInputComponent()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGamePlayerController::Move);
+}
+
+void AGamePlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	APawn* ControlledPawn = GetPawn<APawn>();
+	if (!ControlledPawn)
+		return;
+
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+	ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 }

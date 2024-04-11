02fbf1ca177beac6136c50f366efb4aec1a95b8c
RaulSerranoDev <raulserranodev@gmail.com>
2024-04-11 08:52:14 +0200

    Initializing Vital Attributes

--- Source/Aura/Private/Character/CharacterBase.cpp
+++ Source/Aura/Private/Character/CharacterBase.cpp
@@ -43,4 +43,5 @@ void ACharacterBase::InitializeDefaultAttributes() const
 {
 	ApplyEffectToSelf(DefaultPrimaryAttributes, 1.f);
 	ApplyEffectToSelf(DefaultSecondaryAttributes, 1.f);
+	ApplyEffectToSelf(DefaultVitalAttributes, 1.f);
 }

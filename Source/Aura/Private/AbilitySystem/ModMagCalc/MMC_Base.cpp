ccbbfdb0f198f476676b5ad01339e66e5ce9e0a3
RaulSerranoDev <raulserranodev@gmail.com>
2024-04-11 08:42:25 +0200

    Modifier Magnitude Calculations Refactor

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_Base.cpp
@@ -0,0 +1,52 @@
+// Copyright RaulSerranoDev
+
+
+#include "AbilitySystem/ModMagCalc/MMC_Base.h"
+
+#include "AbilitySystem/GameAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_Base::UMMC_Base()
+{
+}
+
+float UMMC_Base::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather tags from source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvalParams;
+	EvalParams.SourceTags = SourceTags;
+	EvalParams.TargetTags = TargetTags;
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	float AttributeContribution = 0.f;
+
+	int32 index = 0;
+	for (FAttributeCoefficient AttributeCoefficient : AttributeCoefficients)
+	{
+		if (const FGameplayEffectAttributeCaptureDefinition* CapturedAttribute = RelevantAttributesToCapture.
+			FindByPredicate([AttributeCoefficient](const FGameplayEffectAttributeCaptureDefinition& Item)
+				{
+					return Item.AttributeToCapture.AttributeName == AttributeCoefficient.Attribute.AttributeName;
+				}))
+		{
+			float AttValue = 0.f;
+			GetCapturedAttributeMagnitude(*CapturedAttribute, Spec, EvalParams, AttValue);
+			AttValue = FMath::Max<float>(AttValue, 0.f);
+			switch (AttributeCoefficient.Operation)
+			{
+			case EGameplayModOp::Additive:AttributeContribution += (AttributeCoefficient.Coefficient + AttValue); break;
+			case EGameplayModOp::Multiplicitive:AttributeContribution += (AttributeCoefficient.Coefficient * AttValue); break;
+			case EGameplayModOp::Division:AttributeContribution += (AttributeCoefficient.Coefficient / AttValue); break;
+			default:;
+			}
+		}
+	}
+
+	// BaseValue + Coeff1*Att1 ... + LevelMult * PlayerLevel
+	return BaseValue + AttributeContribution + LevelMultiplier * PlayerLevel;
+}
---
+++ Source/Aura/Public/AbilitySystem/ModMagCalc/MMC_Base.h
@@ -0,0 +1,50 @@
+// Copyright RaulSerranoDev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_Base.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAttributeCoefficient
+{
+	GENERATED_BODY()
+	FAttributeCoefficient() {}
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttribute Attribute;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Coefficient = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ValidEnumValues = "Additive, Multiplicitive, Division"))
+	TEnumAsByte<EGameplayModOp::Type> Operation = EGameplayModOp::Additive;
+};
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UMMC_Base : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UMMC_Base();
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	float BaseValue = 1.f;
+
+	UPROPERTY(EditAnywhere)
+	TArray<FAttributeCoefficient> AttributeCoefficients;
+
+	UPROPERTY(EditAnywhere)
+	float LevelMultiplier = 10.f;
+
+};

41075608f34a1a26a0c0ad79d1d5347b7acd0f2e
RaulSerranoDev <raulserranodev@gmail.com>
2024-04-23 08:55:49 +0200

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright RaulSerranoDev
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInfo.FindChecked(CharacterClass);
+}

3db745618f3a23c433dde5fb3f0182c64c72e138
RaulSerranoDev <raulserranodev@gmail.com>
2024-08-08 12:08:29 +0200

    Aura Passive Ability

---
+++ Source/Aura/Private/AbilitySystem/Abilities/PassiveAbility.cpp
@@ -0,0 +1,26 @@
+// Copyright RaulSerranoDev
+
+
+#include "AbilitySystem/Abilities/PassiveAbility.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+
+#include "AbilitySystem/GameAbilitySystemComponent.h"
+
+void UPassiveAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (UGameAbilitySystemComponent* ASC = Cast<UGameAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo())))
+	{
+		ASC->DeactivatePassiveAbility.AddUObject(this, &ThisClass::ReceiveDeactivate);
+	}
+}
+
+void UPassiveAbility::ReceiveDeactivate(const FGameplayTag& AbilityTag)
+{
+	if (AbilityTags.HasTagExact(AbilityTag))
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+	}
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/PassiveAbility.h
@@ -0,0 +1,23 @@
+// Copyright RaulSerranoDev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GameGameplayAbility.h"
+#include "PassiveAbility.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UPassiveAbility : public UGameGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+private:
+	void ReceiveDeactivate(const FGameplayTag& AbilityTag);
+
+};

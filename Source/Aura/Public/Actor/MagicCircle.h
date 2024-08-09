bbb7c8ea38176930cce1d2ab5e5ba2de39885f39
RaulSerranoDev <raulserranodev@gmail.com>
2024-08-09 08:58:09 +0200

    Magic Circle

---
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -0,0 +1,27 @@
+// Copyright RaulSerranoDev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class AURA_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMagicCircle();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+
+};
